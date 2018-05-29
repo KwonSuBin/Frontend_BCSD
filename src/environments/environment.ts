942be3bcfe20b356d5bf301ccfc1276125e7efc9
KwonSuBin <kwonsubin@KwonSuBinui-MacBook-Pro.local>
2018-05-29 21:47:36 +0900

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    production: false,
-    firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
-    }
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCRKeYK6BLpny7q57cI_x36-Uv0JToTIbQ",
+    authDomain: "instar-50e2d.firebaseapp.com",
+    databaseURL: "https://instar-50e2d.firebaseio.com",
+    projectId: "instar-50e2d",
+    storageBucket: "gs://instar-50e2d.appspot.com/",
+    messagingSenderId: "16842737420"
+  }
 };
